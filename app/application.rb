391cdf3e8d0b491c72e85fb96793c886331a04ff
Isaac Villicana <villicana.i@gmail.com>
2018-06-12 19:31:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,12 @@
 class Application
-  @@item = []
+  @@items = []
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items\//)
       item_name = req.path.split(/items\//).last
-
+      item = @@items.find {|i| i.name == item_name}
       if Item.all.include? item
         resp.write "hi"
       else

4e7d9e5884919393d8af4417d3fcb8dc758d627f
Isaac Villicana <villicana.i@gmail.com>
2018-06-12 19:22:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,19 @@
 class Application
   def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
+    if req.path.match(/items\//)
+      item_name = req.path.split(/items\//).last
+      item = item.
+      if Item.all.include? item
+        resp.write "#"
+      else
+
+      end
+    else
+      resp.status = 404
+      resp.write
+    end
   end
 end
